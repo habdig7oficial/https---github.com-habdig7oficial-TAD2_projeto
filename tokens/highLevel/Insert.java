fb290472866b0d8183010db91dc18d512df63b57
Mateus Felipe da Silveira Vieira <10723904@educa.mackenzie.br>
2025-11-18 11:03:46 -0300

    mackenzie

--- tokens/highLevel/Insert.java
+++ tokens/highLevel/Insert.java
@@ -1,8 +1,21 @@
 package tokens.highLevel;
 
+import java.util.Arrays;
+
+import lib.Searcher;
 import lib.DataStructs.LinkedList;
 import tokens.BaseToken;
+import tokens.asm.Add;
+import tokens.asm.Dec;
+import tokens.asm.Div;
+import tokens.asm.Inc;
 import tokens.asm.Instruction;
+import tokens.asm.InstructionSet;
+import tokens.asm.Jnz;
+import tokens.asm.Mov;
+import tokens.asm.Mul;
+import tokens.asm.Out;
+import tokens.asm.Sub;
 
 public class Insert extends BaseHighLevel {
     public Insert(String name, short argsMin, short argsMax, LinkedList<Instruction> asmList){
@@ -11,7 +24,50 @@ public class Insert extends BaseHighLevel {
 
     @Override
     protected void callMethod(String... args) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'callMethod'");
+        Searcher<Instruction, String> s = new Searcher<Instruction, String>();
+
+        short location = s.search(InstructionSet.instructionSet, args[0].trim().toUpperCase());
+        if(location != -1){
+            try {
+                Instruction newInstruction = null;
+                switch (InstructionSet.instructionSet[location]) {
+                    case Mov n:
+                            newInstruction = new Mov(n.getName(), i, Arrays.copyOfRange(asmCommands, 1, asmCommands.length));
+                        break;
+                    case Inc n:
+                            newInstruction = new Inc(n.getName(), i, Arrays.copyOfRange(asmCommands, 1, asmCommands.length));
+                        break;
+                    case Dec n:
+                            newInstruction = new Dec(n.getName(), i, Arrays.copyOfRange(asmCommands, 1, asmCommands.length));
+                        break;
+                    case Add n:
+                            newInstruction = new Add(n.getName(), i, Arrays.copyOfRange(asmCommands, 1, asmCommands.length));
+                        break;
+                    case Sub n:
+                            newInstruction = new Sub(n.getName(), i, Arrays.copyOfRange(asmCommands, 1, asmCommands.length));
+                        break;
+                    case Mul n:
+                            newInstruction = new Mul(n.getName(), i, Arrays.copyOfRange(asmCommands, 1, asmCommands.length));
+                        break;
+                    case Div n:
+                            newInstruction = new Div(n.getName(), i, Arrays.copyOfRange(asmCommands, 1, asmCommands.length));
+                        break;
+                    case Jnz n:
+                            newInstruction = new Jnz(n.getName(), i, Arrays.copyOfRange(asmCommands, 1, asmCommands.length));
+                        break;
+                    case Out n:
+                            newInstruction = new Out(n.getName(), i, Arrays.copyOfRange(asmCommands, 1, asmCommands.length));
+                        break;
+                    default:
+                }
+                this.getAsmList().append(newInstruction);
+                System.out.println(newInstruction);
+            } 
+            catch (Exception e) {
+                        System.out.println("--- ERROR ---\n" + e.getMessage());
+            }
+        }
+        else
+            System.out.println("--- ERROR ---\nInvalid Command!");
     }
 }
